c4ab2cd8a4b9b376590929113b7ca52a63116e3b
rmyamashi <rmcyamashita@gmail.com>
2024-10-22 17:43:24 +0800

    update api definitions, add put and delete endpoints

---
+++ app/lib/types/airdnd.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from 'mongodb';
+
+export type EventType = {
+    id: ObjectId;
+    name: string;
+    location: string;
+    coordinates: {
+        lat: number;
+        lng: number;
+    };
+    gameType: string;
+    date: string;
+    participants: number;
+    users: string[];
+};
+
+export type UserType = {
+    id: ObjectId;
+    fname: string;
+    lname: string;
+    age: number;
+    email: string;
+    role: string;
+    rating: number;
+};

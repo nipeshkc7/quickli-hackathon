13e02820f7044c6f21385a51fc92db252f87a8bc
rmyamashi <rmcyamashita@gmail.com>
2024-10-23 09:22:57 +0800

    make create events show in main page, dbname consistency , and coords saving

--- app/lib/types/airdnd.ts
+++ app/lib/types/airdnd.ts
@@ -1,25 +1,26 @@
-import { ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb'
 
 export type EventType = {
-    id: ObjectId;
-    name: string;
-    location: string;
+    id: number,
+    name: string
+    location: string
     coordinates: {
-        lat: number;
-        lng: number;
-    };
-    gameType: string;
-    date: string;
-    participants: number;
-    users: string[];
-};
+        lat: number
+        lng: number
+    }
+    gameType: string
+    date: string
+    participants: number
+    image: string
+    users: string[]
+}
 
 export type UserType = {
-    id: ObjectId;
-    fname: string;
-    lname: string;
-    age: number;
-    email: string;
-    role: string;
-    rating: number;
-};
+    id: ObjectId
+    fname: string
+    lname: string
+    age: number
+    email: string
+    role: string
+    rating: number
+}
